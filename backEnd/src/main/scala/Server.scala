c37bc5df95fdfec07b6647a933beaf238543956b
alexandrustana <alexandrustana2@gmail.com>
2018-04-30 13:10:02 +0300

    Refactor server

--- backEnd/src/main/scala/Server.scala
+++ backEnd/src/main/scala/Server.scala
@@ -1,17 +1,17 @@
+import cats.Monad
 import cats.effect._
 import com.sksamuel.elastic4s.http.HttpClient
-import config.{BackendConfig, DatabaseConfig}
-import domain.account.{AccountService, AccountValidationInterpreter}
+import config.{BackendConfig,                DatabaseConfig}
+import domain.account.{AccountService,       AccountValidationInterpreter}
 import domain.assignment.{AssignmentService, AssignmentValidationInterpreter}
-import domain.course.{CourseService, CourseValidationInterpreter}
-import domain.professor.{ProfessorService, ProfessorValidationInterpreter}
-import domain.student.{StudentService, StudentValidationInterpreter}
+import domain.course.{CourseService,         CourseValidationInterpreter}
+import domain.professor.{ProfessorService,   ProfessorValidationInterpreter}
+import domain.student.{StudentService,       StudentValidationInterpreter}
 import doobie.hikari.HikariTransactor
 import fs2.StreamApp.ExitCode
 import fs2.{Stream, StreamApp}
 import infrastructure.endpoint._
 import infrastructure.repository.doobie._
-import infrastructure.repository.elastic
 import infrastructure.repository.elastic._
 import org.http4s.server.blaze.BlazeBuilder
 
@@ -31,26 +31,21 @@ object Server extends StreamApp[IO] {
       xa   <- Stream.eval(DatabaseConfig.dbTransactor(conf.db))
       es   <- Stream.eval(DatabaseConfig.initializeElasticDb(conf.db))
       _    <- Stream.eval(DatabaseConfig.initializeSQLDb(conf.db, xa))
-      sqlAccountRepo        = DoobieAccountRepositoryInterpreter[F](xa)
-      sqlProfessorRepo      = DoobieProfessorRepositoryInterpreter[F](xa)
-      sqlStudentRepo        = DoobieStudentRepositoryInterpreter[F](xa)
-      sqlCourseRepo         = DoobieCourseRepositoryInterpreter[F](xa)
-      sqlAssignmentRepo     = DoobieAssignmentRepositoryInterpreter[F](xa)
-      elasticAccountRepo    = ElasticAccountRepositoryInterpreter[F](es)
-      elasticProfessorRepo  = ElasticProfessorRepositoryInterpreter[F](es)
-      elasticStudentRepo    = ElasticStudentRepositoryInterpreter[F](es)
-      elasticCourseRepo     = ElasticCourseRepositoryInterpreter[F](es)
-      elasticAssignmentRepo = ElasticAssignmentRepositoryInterpreter[F](es)
-      accountValidation     = AccountValidationInterpreter[F](elasticAccountRepo)
-      professorValidation   = ProfessorValidationInterpreter[F](sqlProfessorRepo)
-      studentValidation     = StudentValidationInterpreter[F](sqlStudentRepo)
-      courseValidation      = CourseValidationInterpreter[F](sqlCourseRepo)
-      assignmentValidation  = AssignmentValidationInterpreter[F](sqlAssignmentRepo)
-      accountService        = AccountService[F](elasticAccountRepo, accountValidation)
-      professorService      = ProfessorService[F](sqlProfessorRepo, professorValidation)
-      studentService        = StudentService[F](sqlStudentRepo, studentValidation)
-      courseService         = CourseService[F](sqlCourseRepo, courseValidation)
-      assignmentService     = AssignmentService[F](sqlAssignmentRepo, courseValidation, assignmentValidation)
+      accountRepo          = accountRepository[F](Left(xa))
+      professorRepo        = professorRepository[F](Left(xa))
+      studentRepo          = studentRepository[F](Left(xa))
+      courseRepo           = courseRepository[F](Left(xa))
+      assignmentRepo       = assignmentRepository[F](Left(xa))
+      accountValidation    = AccountValidationInterpreter[F](accountRepo)
+      professorValidation  = ProfessorValidationInterpreter[F](professorRepo)
+      studentValidation    = StudentValidationInterpreter[F](studentRepo)
+      courseValidation     = CourseValidationInterpreter[F](courseRepo)
+      assignmentValidation = AssignmentValidationInterpreter[F](assignmentRepo)
+      accountService       = AccountService[F](accountRepo, accountValidation)
+      professorService     = ProfessorService[F](professorRepo, professorValidation)
+      studentService       = StudentService[F](studentRepo, studentValidation)
+      courseService        = CourseService[F](courseRepo, courseValidation)
+      assignmentService    = AssignmentService[F](assignmentRepo, courseValidation, assignmentValidation)
       exitCode <- BlazeBuilder[F]
                    .bindHttp(8080, "localhost")
                    .mountService(AccountEndpoints(accountService), "/")
@@ -60,4 +55,29 @@ object Server extends StreamApp[IO] {
                    .mountService(AssignmentEndpoints(assignmentService), "/")
                    .serve
     } yield exitCode
+
+  private def accountRepository[F[_]: Monad](conf: Either[HikariTransactor[F], HttpClient]) = conf match {
+    case Left(xa)  => DoobieAccountRepositoryInterpreter[F](xa)
+    case Right(es) => ElasticAccountRepositoryInterpreter[F](es)
+  }
+
+  private def professorRepository[F[_]: Monad](conf: Either[HikariTransactor[F], HttpClient]) = conf match {
+    case Left(xa)  => DoobieProfessorRepositoryInterpreter[F](xa)
+    case Right(es) => ElasticProfessorRepositoryInterpreter[F](es)
+  }
+
+  private def studentRepository[F[_]: Monad](conf: Either[HikariTransactor[F], HttpClient]) = conf match {
+    case Left(xa)  => DoobieStudentRepositoryInterpreter[F](xa)
+    case Right(es) => ElasticStudentRepositoryInterpreter[F](es)
+  }
+
+  private def courseRepository[F[_]: Monad](conf: Either[HikariTransactor[F], HttpClient]) = conf match {
+    case Left(xa)  => DoobieCourseRepositoryInterpreter[F](xa)
+    case Right(es) => ElasticCourseRepositoryInterpreter[F](es)
+  }
+
+  private def assignmentRepository[F[_]: Monad](conf: Either[HikariTransactor[F], HttpClient]) = conf match {
+    case Left(xa)  => DoobieAssignmentRepositoryInterpreter[F](xa)
+    case Right(es) => ElasticAssignmentRepositoryInterpreter[F](es)
+  }
 }
