a27c6210c1c681942ac8704341b8713a8eac67be
alexandrustana <alexandrustana2@gmail.com>
2018-05-02 22:52:51 +0300

    Add submission case class

---
+++ backEnd/src/main/scala/domain/submission/Submission.scala
@@ -0,0 +1,18 @@
+package domain.submission
+
+import java.time.LocalDate
+
+import domain.assignment.Assignment
+import domain.student.Student
+
+/**
+  * @author Alexandru Stana, alexandru.stana@busymachines.com
+  * @since 02/05/2018
+  */
+case class Submission(
+  id:         Option[Long] = None,
+  student:    Option[Student] = None,
+  assignment: Option[Assignment] = None,
+  grade:      Int,
+  time:       LocalDate
+)
